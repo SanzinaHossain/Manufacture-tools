c523d541b2a5ac80b10b2b1f7f6162deec7e1c1a
SanzinaHossain <sanzina67hossain@gmail.com>
2022-05-28 13:11:26 +0600

    delete tools done

--- src/Pages/DashBoard/ManageItems.js
+++ src/Pages/DashBoard/ManageItems.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading/Loading';
+import ProductTable from './ProductTable';
 
 const ManageItems = () => {
+  const { data: tools, isLoading, refetch } = useQuery('tools', () => fetch('http://localhost:5000/tools', {
+    method: 'GET',
+    headers:{
+        authorization: `Bearer ${localStorage.getItem('accessToken')}`
+    }
+}).then(res => res.json()));
+if (isLoading) {
+    return <Loading></Loading>
+}
   return (
-    <div>ManageItems</div>
+    <div className='mt-5'>
+        <h1 className='text-center text-3xl text-primary font-bold'>All Products</h1>
+        <div class="overflow-x-auto mt-5">
+  <table class="table w-full">
+    <thead>
+      <tr class="bg-green-300">
+        <th>Product ID</th>
+        <th>Product Name</th>
+        <th>Price</th>
+        <th>Stock</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+          tools.map(t=>
+          <ProductTable
+              t={t}
+              refetch={refetch}
+          ></ProductTable>
+            )
+      }
+    </tbody>
+  </table>
+</div>
+        </div>
   )
 }
 
---
+++ src/Pages/DashBoard/ProductTable.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { toast } from 'react-toastify';
+
+const ProductTable = ({t,refetch}) => {
+    const {_id,name,stock,price}=t;
+  const DeleteItems=(id)=>{
+      console.log(id)
+      const proceed=window.confirm('Are you sure?')
+      if(proceed)
+      {
+          const url=`http://localhost:5000/tools/${id}`;
+          fetch(url,{
+              method:'DELETE'
+          })
+          .then(res=>res.json())
+          .then(data=>{
+              toast("successfully delete items")
+              refetch();
+          })
+        }
+}
+
+  return (
+    <tr>
+    <td class="text-black" >{_id}</td>
+    <td class="text-black" >{name}</td>
+    <td class="text-black" >{price}</td>
+    <td class="text-black" >{stock}</td>
+    <td><button onClick={()=>DeleteItems(_id)} class="btn btn-sm btn-primary">Delete Product</button></td>
+  </tr>
+  )
+}
+
+export default ProductTable
