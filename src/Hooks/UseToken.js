444c91623c2ad23cfaec021da4e6912202394970
SanzinaHossain <sanzina67hossain@gmail.com>
2022-06-01 20:18:26 +0600

    link set

--- src/Hooks/UseToken.js
+++ src/Hooks/UseToken.js
@@ -7,7 +7,7 @@ const useToken = (user) => {
      const currentuser={email:email}
      if(email)
      {
-         fetch(`http://localhost:5000/user/${email}`,{
+         fetch(`https://morning-fortress-41399.herokuapp.com/user/${email}`,{
            method:'PUT',
            headers:{
                'content-type':'application/json'
